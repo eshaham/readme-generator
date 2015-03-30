2050e8f6076c3a1c7abbc19a1ac584ea6321fccc
Elad Shaham <elad.shaham@gmail.com>
2015-03-30 17:35:07 +0300

    also concat bower files when in production

--- gulp/tasks/prepScripts.js
+++ gulp/tasks/prepScripts.js
@@ -1,6 +1,9 @@
 'use strict';
 
 var gulp = require('gulp'),
+    gulpFilter = require('gulp-filter'),
+    streamqueue = require('streamqueue'),
+    bowerFiles = require('main-bower-files'),
 	gulpif = require('gulp-if'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify');
@@ -8,7 +11,12 @@ var gulp = require('gulp'),
 var env = process.env.NODE_ENV  || 'development';
 
 gulp.task('prep-scripts', ['clean'], function() {
-    return gulp.src(['./app/app.js', './app/modules/*/*.js', './app/**/*.js', '!./app/lib/**'], { base: './app/' })
+    return streamqueue(
+        { objectMode: true },
+        gulp.src(bowerFiles({ paths: { bowerDirectory: './app/lib' }}), { base: './app/' }),
+        gulp.src(['./app/app.js', './app/modules/*/*.js', './app/**/*.js', '!./app/lib/**'], { base: './app/' })
+        )
+        .pipe(gulpFilter('**/*.js'))
         .pipe(gulpif(env === 'production', concat('app.js')))
         .pipe(gulpif(env === 'production', uglify()))
         .pipe(gulp.dest('./dist/'));
