6a02637535f01c4bbd9cf9a9684bbc03c6d60d89
Elad Shaham <elad.shaham@gmail.com>
2015-04-03 17:05:34 +0300

    make bower files work under cdn for production

--- gulp/tasks/prepIndex.js
+++ gulp/tasks/prepIndex.js
@@ -1,8 +1,12 @@
 'use strict';
 
 var gulp = require('gulp'),
+    gulpif = require('gulp-if'),
     bowerFiles = require('main-bower-files'),
-    inject = require('gulp-inject');
+    inject = require('gulp-inject'),
+    cdnizer = require('gulp-cdnizer');
+
+var env = process.env.NODE_ENV  || 'development';
 
 // prepare index.html file
 gulp.task('prep-index', ['prep-styles', 'prep-scripts', 'copy-bower-components', 'copy-html-files'], function () {
@@ -13,5 +17,12 @@ gulp.task('prep-index', ['prep-styles', 'prep-scripts', 'copy-bower-components',
         .pipe(inject(gulp.src(bowerFiles(bowerOptions), { read: false }), { name: 'bower', ignorePath: 'dist' }))
         .pipe(inject(gulp.src(['./dist/**/*.css', '!./dist/lib/**'], { read: false }), { ignorePath: 'dist' }))
         .pipe(inject(gulp.src(['./dist/**/*.js', '!./dist/lib/**'], { read: false }), { ignorePath: 'dist' }))
+        .pipe(gulpif(env === 'production', cdnizer([
+            'cdnjs:angular.js', 
+            { 
+                cdn: 'cdnjs:angular-ui-router',
+                test: 'window.angular'
+            }
+        ])))
         .pipe(gulp.dest('./dist'));
 });
