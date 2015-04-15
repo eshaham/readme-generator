5b08b5c8be967e1eedc683ea7837f865d2507dce
Elad Shaham <elad.shaham@gmail.com>
2015-04-15 14:09:14 +0300

    add generate module and create initial generate view

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('readme', ['readme.core', 'readme.home']);
+angular.module('readme', ['readme.core', 'readme.home', 'readme.generate']);
 
 // Setting HTML5 Location Mode
 angular.module('readme').config(['$locationProvider', 
